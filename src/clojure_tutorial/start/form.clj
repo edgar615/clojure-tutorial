f7979011b42095f81e7e5301a9c2a66b53be09bb
Edgar Zhang <edgar615@gmail.com>
2015-03-25 22:13:41 +0800

    map解构

--- src/clojure_tutorial/start/form.clj
+++ src/clojure_tutorial/start/form.clj
@@ -84,3 +84,67 @@ p
   (conj original-vector (+ x z)))                           ;[42 "foo" 99.2 [5 12] 141.2]
 ;这里的original-vector被绑定到未做修改的集合v
 
+;map解构
+;map解构对于下面的几种数据结构有效
+;Clojure原生的hash-map、array-map，以及记录类型
+;任何实现了java.util.Map的对象
+;get方法所支持的任何对象，比如：Clojure原生vector，字符串、数组
+;map解构
+(def m {:a 5 :b 6
+        :c [7 8 9]
+        :d {:e 10 :f 11}
+        "foo" 88
+        42 false})
+(let [{a :a b :b} m]
+  (+ a b))                                                  ;11
+;可以在map解构中用做key的不止是关键字，可以是任何类型的值，比如字符串
+(let [{f "foo"} m]
+  (+ f 12))                                                 ;100
+(let [{v 42} m]
+  (if v 1 0))                                               ;0
+;如果要进行map解构的是vector，字符串或者数组的话，那么解构的则是数字类型的数组下标。
+(let [{x 3 y 8} [12 0 0 -18 44 6 0 0 1]]
+  (+ x y))                                                  ;-17
+;map解构也可以处理内嵌map
+(let [{{e :e} :d} m]
+  (* 2 e))                                                  ;20
+;可以把顺序解构和map解构结合起来
+(let [{[x _ y] :c} m]
+  (+ x y))                                                  ;16
+(def map-in-vector ["Edgar" {:birthday (java.util.Date. 73 1 6)}])
+(let [[name {bd :birthday}] map-in-vector]
+  (str name " was born on " bd))                            ;"Edgar was born on Tue Feb 06 00:00:00 CST 1973"
+;保持被解构的集合
+(let [{a :a :as original-map} m]
+  (assoc original-map :name "Edgar"))                       ;{"foo" 88, :name "Edgar", :c [7 8 9], :b 6, :d {:e 10, :f 11}, 42 false, :a 5}
+;默认值
+;可以使用:or来提供一个默认的map，如果要解构的key在集合中没有的话，那么默认map中的值会作为默认值绑定到我们的解构符号上去
+(let [{k :unknown x :a :or {k 50}} m]
+  (+ k x))                                                  ;55
+;上述代码等同于
+(let [{k :unknown x :a} m
+      k (or k 50)]
+  (+ k x))
+;:or能区分到底是美柚赋值，还是赋给的值就是逻辑false(nil或者false)
+(let [{opt1 :option} {:option false}
+      opt1 (or opt1 true)
+      {opt2 :option :or {:opt2 true}} {:option false}]
+  {:opt1 opt1 :opt2 opt2})
+;绑定符号到map中同名关键字所对应的元素
+(def chas {:name "Chas" :age 31 :location "Mass"})
+(let [{name :name age :age location :location} chas]
+  (format "%s is %s years old and lives in %s" name age location)) ;"Chas is 31 years old and lives in Mass"
+;上述代码很冗长，这种情况下，Clojure提供了:keys、:strs和:syms来指定map中key的类型
+;:keys表示key的类型是关键字，:strs表示key的类型是字符串，:syms表示key的类型是符号
+;:keys
+(let [{:keys [name age location]} chas]
+  (format "%s is %s years old and lives in %s" name age location))
+;:strs
+(def brian {"name" "Brain" "age" 31 "location" "British"})
+(let [{:strs [name age location]} brian]
+  (format "%s is %s years old and lives in %s" name age location)) ;"Brain is 31 years old and lives in British"
+;:syms
+(def christophe {'name "Christophe" 'age 33 'location "Rhone"})
+(let [{:syms [name age location]} christophe]
+  (format "%s is %s years old and lives in %s" name age location)) ;"Christophe is 33 years old and lives in Rhone"
+
