52169684e922fce70f8d4d565750791f6a55867c
Edgar Zhang <edgar615@gmail.com>
2015-12-15 22:36:25 +0800

    解构

--- src/clojure_tutorial/api/core/defn.clj
+++ src/clojure_tutorial/api/core/defn.clj
@@ -64,3 +64,91 @@
 
 (x-chop "Kanye East")
 ;; "I karate chop Kanye East! Take that!"
+
+;; Clojure also allows you to define variable-arity functions by including a rest parameter,
+;; as in “put the rest of these arguments in a list with the following name.”
+;; The rest parameter is indicated by an ampersand (&)
+
+(defn codger-communication
+  [whippersnapper]
+  (str "Get off my lawn" whippersnapper "!!!"))
+
+(defn codger
+  [& whippersnappers]
+  (map codger-communication whippersnappers))
+
+(codger "Billy" "Anne-Marie" "The Incredible Bulk")
+;; => ("Get off my lawnBilly!!!" "Get off my lawnAnne-Marie!!!" "Get off my lawnThe Incredible Bulk!!!")
+
+;; You can mix rest parameters with normal parameters, but the rest parameter has to come last:
+(defn favorite-things
+  [name & things]
+  (str "Hi, " name ", here are my favorite things: "
+       (clojure.string/join "," things)))
+
+(favorite-things "Doreen" "gum" "shoes" "kara-te")
+;; => "Hi, Doreen, here are my favorite things: gum,shoes,kara-te"
+
+;; Destructuring
+(defn my-first
+  [[first-thing]]
+  first-thing)
+
+(my-first ["oven" "bike" "war-axe"])
+;; => "oven"
+
+(defn chooser
+  [[first-choice second-choice & unimportant-choices]]
+  (println (str "Your first choice is: " first-choice))
+  (println (str "Your second choice is: " second-choice))
+  (println (str "We're ignoring the rest of your choices. "
+                "Here they are in case you need to cry over them: "
+                (clojure.string/join ", " unimportant-choices))))
+
+(chooser ["Marmalade", "Handsome Jack", "Pigpen", "Aquaman"])
+
+;; Destructuring map
+(defn announce-treasure-location
+  [{lat :lat lng :lng}]
+  (println (str "Treasure lat: " lat))
+  (println (str "Treasure lng: " lng)))
+
+(announce-treasure-location {:lat 28.22 :lng 81.33})
+
+;; We often want to just break keywords out of a map, so there’s a shorter syntax for that.
+;; This has the same result as the previous example:
+(defn announce-treasure-location2
+  [{:keys [lat lng]}]
+  (println (str "Treasure lat: " lat))
+  (println (str "Treasure lng: " lng)))
+
+;; You can retain access to the original map argument by using the :as keyword.
+;; In the following example, the original map is accessed with treasure-location:
+
+(defn receive-treasure-location
+  [{:keys [lat lng] :as treasure-location}]
+  (println (str "Treasure lat: " lat))
+  (println (str "Treasure lng: " lng))
+
+  ;; One would assume that this would put in new coordinates for your ship
+  (:lat treasure-location))
+
+;; Function Body
+;; The function body can contain forms of any kind. Clojure automatically returns the last form evaluated.
+(defn illustrative-function
+  []
+  (+ 1 304)
+  30
+  "joe")
+(illustrative-function)
+;; => joe
+
+(defn number-comment
+  [x]
+  (if (> x 6)
+    "Oh my gosh! What a big number!"
+    "That number's OK, I guess"))
+(number-comment 5)
+;; "That number's OK, I guess"
+(number-comment 7)
+;; "Oh my gosh! What a big number!"
